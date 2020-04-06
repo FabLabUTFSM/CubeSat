dec39fcb1bacc932c84cd491f1cc457cfbd6a893
josetomas <josetodf@gmail.com>
2020-04-06 18:36:34 -0400

    cosigo Json V1

---
+++ JsonV1/JsonV1.ino
@@ -0,0 +1,19 @@
+#include <ArduinoJson.h>
+DynamicJsonDocument doc(42);
+
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  JsonArray obj1 = doc.createNestedArray("obj1");
+  JsonArray obj2 = doc.createNestedArray("obj2");
+  obj1.add(random(300));
+  obj2.add(random(300));
+  obj2.add(random(300));
+  serializeJson(doc, Serial);
+  Serial.println(" ");
+  doc.clear();
+}
